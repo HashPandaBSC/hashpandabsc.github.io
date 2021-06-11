e3540f2b26b4042b3d34f63c35bd293373ecee2d
PandaBSCDev <83962757+PandaBSCDev@users.noreply.github.com>
2021-06-11 21:23:29 +0100

    Add files via upload

---
+++ js/i18n.js
@@ -0,0 +1,80 @@
+var translations = {
+    'id': {
+        'ltr': true,
+        'join-telegram': 'Gabung Telegram',
+        'buy-pancakeswap': 'Beli di PancakeSwapV2',
+        'title': 'Meme Token Desentralisasi',
+        'subtitle': 'HashPanda ingin menjadi meme-token paling desentralisasi sepanjang masa, mendistribusikan tokennya ke lebih dari 1 juta pengguna dan melampaui jumlah pemegang DogeCoin',
+        'num-holders': 'Lebih dari 256,527 pemegang token di Binance Smart Chain',
+        'features-title': 'Fitur #PANDA',
+        'features-list': 'Penguncian likuiditas otomatis, yield-farming, dan utilitas dApp',
+        'token-title': 'Token',
+        'token-description': 'Fitur token yang menguntungkan pengguna dan pemegang token.',
+        'token-b1': 'Memiliki Panda',
+        'token-b2': 'Utilitas untuk dApp',
+        'token-b3': 'Farming auto-yield',
+        'distribution-title': 'Distribusi',
+        'distribution-description': 'Distribusi desentralisasi menggunakan mekanisme komunitas.',
+        'distribution-b1': 'Distribusi terprogram',
+        'distribution-b2': 'Hadiah komunitas',
+        'distribution-b3': 'Memberi tip kepada pengguna/kreator',
+        'benefits-title': 'Manfaat',
+        'benefits-description': 'Manfaat tak terhitung untuk pemegang token HashPanda.',
+        'benefits-b1': 'Didistribusikan secara adil',
+        'benefits-b2': 'Dasar harga naik',
+        'benefits-b3': 'Keamanan anti-whale',
+        'project-title': 'Proyek #HashPanda',
+        'project-text': 'Misi HashPanda adalah menjadi meme-token paling terdesentralisasi dan paling terdistribusi adil di dunia. Kami telah memelopori mekanisme distribusi yang memungkinkan kami mendistribusikan token secara sosial, mencegah whale-dump sehingga menciptakan aset safe-haven yang dapat berfungsi sebagai celengan selama bertahun-tahun ke depan. Selain itu, pemegang token kami menerima 3% dari semua transaksi, memberikan manfaat farming yield otomatis.',
+        'problem-title': 'Masalah',
+        'problem-text1': 'HashPanda memecahkan masalah distribusi token tidak adil dan whale dump yang sering. Dogecoin, misalnya: pemegang terbesar memiliki 28% dari semua token. Pemegang ini saja dapat menghancurkan harga sekitar 96%.',
+        'problem-text2': 'Kebanyakan token lain memiliki distribusi suplai yang tidak merata. Untuk token ini, segelintir individu memiliki sebagian besar suplai, mengakibatkan dump terus menerus dan tekanan harga yang tidak stabil dalam jangka panjang.',
+        'solution-title': 'Solusinya',
+        'solution-text1': 'HashPanda memecahkan masalah ini melalui mekanisme distribusi sosial inovatif. Mekanisme ini bertanggung jawab mendistribusikan 80% dari semua token PANDA yang akhirnya beredar ke berbagai pemegang (500,000 hingga 1 juta pemegang token, tepatnya), membuat HashPanda menjadi meme-token paling terdesentralisasi sepanjang masa.',
+        'solution-text2': 'Ini dilakukan dengan berlangganan terprogram ke semua transaksi di Binance Smart Chain (yang bersifat publik) dan secara acak memilih beberapa ribu pemenang setiap hari, yang diberi sejumlah token PANDA. Penerima token PANDA biasanya menjadi pemegang jangka panjang dan pendukung setia proyek.',
+        'method-title': 'Distribusi Terdesentralisasi',
+        'method-text': 'Kami meyakini pendekatan kami adalah cara yang paling adil dan berkelanjutan untuk membuat token terdesentralisasi. Ini membantu memastikan stabilitas nilai token di masa depan dan berfungsi sebagai tindakan anti-whale yang efektif. Anda dapat melacak proses distribusi di bawah. Prosesnya diperkirakan memakan waktu antara 4 dan 8 minggu, hingga suplai yang dialokasikan sebesar 400 triliun token PANDA telah didistribusikan sepenuhnya. Bar kemajuan diperbarui setiap 48 jam.',
+        'bar-text': 'MENDISTRIBUSIKAN PANDA KEPADA 1 JUTA PEMEGANG',
+        'token-holders': 'Pemegang Token',
+        'circulating-supply': 'Suplai yang Beredar',
+        'market-cap': 'Kapitalisasi Pasar',
+        'community-size': 'Ukuran Komunitas',
+        'questions-title': 'Pertanyaan Umum',
+        'question1': 'Bagaimana cara membeli token HashPanda?',
+        'answer1': 'Unduh TrustWallet atau MetaMask, daftar di Binance dan beli BNB. Klik tombol beli di halaman ini dan Anda akan diarahkan ke PancakeSwapV2. Klik "Saya mengerti" dan beli jumlah yang diinginkan.',
+        'question2': 'Apa peta jalan Anda?',
+        'answer2': 'Misi pertama HashPanda adalah menjadi meme-token yang paling terdesentralisasi. Kami ingin melampaui jumlah pemegang token aktif Dogecoin. Misi kedua kami adalah membuat cryptocurrency menjadi mainstream dengan menyediakan dApp yang mudah digunakan oleh “orang normal”. Saat ini, kami sedang mengembangkan platform konten berbagi meme dengan fitur tip kreator. Ini akan memberikan utilitas pada token PANDA dan akan menjamin pertumbuhan berkelanjutan dari proyek ini, setelah misi pertama direalisasikan.',
+        'question3': 'Apa pemasaran untuk HashPanda?',
+        'answer3': 'HashPanda saat ini memiliki enam sukarelawan yang mengerjakan kegiatan pemasaran. Secara khusus, kami sedang mengerjakan kampanye pemasaran gerilya untuk menjangkau audiens mainstream yang luas.',
+        'question4': 'Apa saja tokenomic HashPanda?',
+        'answer4': 'Total suplainya adalah 1,000,000,000,000,000,000 di mana 50% dibakar, 40% didistribusikan melalui airdrop/hadiah komunitas, 5% dialokasikan untuk pertumbuhan/pengembangan, dan 5% disediakan sebagai likuiditas publik. Dari nilai transaksi, 5% dikunci untuk likuiditas dan 3% didistribusikan ke pemegang token.',
+        'question5': 'Mengapa saya menerima token PANDA?',
+        'answer5': 'Silahkan lihat bagian "Distribusi Terdesentralisasi" di halaman ini. Anda telah mendaftar untuk airdrop, menambah nilai proyek, bertransaksi di BSC, bertransaksi dengan PANDA atau menggunakan salah satu dApp kami.',
+        'question6': 'Apa alamat kontrak token HashPanda?',
+        'answer6': 'Alamat resmi BEP-20 (BSC) adalah 0x8578Eb576e126f67913a8bC0622e0A22EBa0989A',
+        'question7': 'Apa token ticker untuk HashPanda?',
+        'answer7': 'Tickernya adalah PANDA',
+        'join-pandagang': 'Gabung #PandaGang',
+    },
+}
+
+function getLang() {
+    var lang = null
+    if (navigator.languages && navigator.languages.length) {
+        lang = navigator.languages[0];
+    } else {
+        lang = navigator.userLanguage || navigator.language || navigator.browserLanguage || 'en';
+    }
+    return lang.split('-')[0]
+}
+
+window.addEventListener('DOMContentLoaded', function(e) {
+    var lang = getLang()
+    if (translations[lang]) {
+        Object.keys(translations[lang]).forEach(function(key) {
+            elems = document.querySelectorAll('[data-i18n="'+key+'"]');
+            for (var i = 0; i < elems.length; i++) {
+                elems[i].innerText = translations[lang][key]
+            }
+        })
+    }
+});
